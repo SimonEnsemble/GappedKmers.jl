80d415a5f22fe315be47cfd39f30316d929032db
Olefincode <mcquiddk@oregonstate.edu>
2024-09-23 16:00:46 -0700

    update

--- src/GappedKmers.jl
+++ src/GappedKmers.jl
@@ -1,9 +1,9 @@
 module GappedKmers
 
-using BioSequences, IterTools
+using BioSequences, IterTools, CairoMakie, NPZ, JLD
 
 include("list_of_gapped_kmers.jl")
 
-export list_of_gapped_kmers, convert_to_regex, string_to_DNA_seq, number_of_gapped_kmers
+export list_of_gapped_kmers, convert_to_regex, string_to_DNA_seq, number_of_gapped_kmers, featurizer, build_feature_matrix, feature_matrix_file_direct, feature_matrix_import, gapped_kmer_kernel, gapped_kmer_kernel_matrix, kernel_matrix_file_name, kernel_matrix_import, viz_kernel_matrix 
 
 end
