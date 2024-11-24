dc3f33cbd8c276c0be1e77c740faede5931455a4
Cory Simon <cory.simon@oregonstate.edu>
2024-11-23 17:51:07 -0800

    clean up

--- src/list_of_gapped_kmers.jl
+++ src/list_of_gapped_kmers.jl
@@ -5,48 +5,45 @@ using BioSequences
     k = 2 # number of informative (non-wildcard) positions (nucleotides)
     number_of_gapped_kmers(ℓ, k)
 
-return the number of possible gapped k-mers.
+returns the number (`Int`) of possible gapped k-mers.
 """
-number_of_gapped_kmers(ℓ::Int, k::Int) = 4^k * binomial(ℓ, k)
+number_of_gapped_kmers(ℓ::Int, k::Int) = 4 ^ k * binomial(ℓ, k)
 
 """
     ℓ = 3 # length of subsequence
     k = 2 # number of informative (non-wildcard) positions (nucleotides)
     list_of_gapped_kmers(ℓ, k)
 
-create and return a vector (`Vector{String}`) of all possible gapped k-mers. the wildcard position in a gapped k-mer is represented by a "-" character.
+create and return a vector (`Vector{String}`) of all possible gapped k-mers.
+the wildcard position in a gapped k-mer is represented by a "-" character.
 """
 function list_of_gapped_kmers(ℓ::Int, k::Int)
     @assert k ≤ ℓ
 
-	# list of gapped kmers
-	gkmers = []
-	# list of possible positions
-	nucleotides = "ATGC-"
-	
-	# function found on https://discourse.julialang.org/t/how-to-create-all-possible-k-mers-with-a-set-of-alphabets/83186
-	for gkmer in join.(Iterators.product(Iterators.repeated(nucleotides, ℓ)...))
-		# if any gkmer produced has ℓ - k wildcards '-', append to list of kmers
-		if count(x -> x == '-', gkmer) == (ℓ - k)
-			push!(gkmers, gkmer)
-		end
-	end
+    # list of possible nucleotides, incl. wildcard
+    nucleotides = "ATGC-"
+    
+    # https://discourse.julialang.org/t/how-to-create-all-possible-k-mers-with-a-set-of-alphabets/83186
+    # list of gapped kmers
+    gkmers = String[]
+    for gkmer in join.(Iterators.product(Iterators.repeated(nucleotides, ℓ)...))
+        # if any gkmer produced has ℓ - k wildcards '-', append to list of kmers
+        if count(x -> x == '-', gkmer) == (ℓ - k)
+            push!(gkmers, gkmer)
+        end
+    end
 
     return gkmers
-	
-	# replace wildcard '-' with "[ATCG]" for BioSequences.jl
-	gkmers = [replace(gkmer, s"-" => s"[ATCG]") for gkmer in gkmers]
-
-	# convert to Regex expressions for searching
-	return [BioSequences.RE.Regex{DNA}(gkmer) for gkmer in gkmers]
 end
 
 """
     convert_to_regex("A-T") # returns a Regex{DNA}
 
-convert a gapped k-mer in the form of a `String`, with the character "-" representing a wildcard position, into a BioSequences regular expression for the purpose of searching DNA sequences for this gapped k-mer.
+converts a gapped k-mer in the form of a `String`, with the character "-" representing 
+a wildcard position, into a `BioSequences` regular expression `Regex{DNA}` for the purpose 
+of searching DNA sequences for this gapped k-mer.
 
-# example
+### example
 ```julia
 using BioSequences
 
@@ -64,8 +61,9 @@ function convert_to_regex(gkmer::String)
 end
 
 """
-	string_to_DNA_seq("ATTTT")
+    string_to_DNA_seq("ATTTT")
 
-converts a DNA sequence represented as a `String` to an efficent 2-bit-per-nucleotide representation via `BioSequences.jl`, specifically, a `LongDNA{2}`.
+converts a DNA sequence represented as a `String` to an efficent 2-bit-per-nucleotide 
+representation via `BioSequences.jl`---specifically, a `LongDNA{2}`.
 """
 string_to_DNA_seq(DNA_seq::String) = LongDNA{2}(DNA_seq)
